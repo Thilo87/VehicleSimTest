24866d305d507290fb8f49c11a09405486711e20
Thilo <nollthilo@hotmail.com>
2024-02-05 14:54:02 +0100

    Worked on TrafficLight, removed TrafficLightController

--- Source/VehicleTest/Private/TrafficRegulationActors/TrafficLight/TrafficLight.cpp
+++ Source/VehicleTest/Private/TrafficRegulationActors/TrafficLight/TrafficLight.cpp
@@ -4,3 +4,97 @@
 #include "TrafficRegulationActors/TrafficLight/TrafficLight.h"
 
 
+float ATrafficLight::GetCurrentPhaseDuration() const
+{
+	return PhaseDurations[ static_cast< uint8 >( CurrentPhase ) ];
+}
+
+ETrafficLightPhase ATrafficLight::GetNextPhase() const
+{
+	return static_cast< ETrafficLightPhase >( ( static_cast< int >( CurrentPhase ) + 1 ) % 4 );
+}
+
+ETrafficLightPhase ATrafficLight::GetPreviousPhase() const
+{
+	return static_cast< ETrafficLightPhase >( ( static_cast< int >( CurrentPhase ) - 1 ) % 4 );
+}
+
+bool ATrafficLight::IsRunning() const
+{
+	return GetWorld()->GetTimerManager().IsTimerActive( PhaseTimer );
+}
+
+void ATrafficLight::OnPhaseTimerTriggered()
+{
+	CurrentPhase = GetNextPhase();
+
+	GetWorld()->GetTimerManager().ClearTimer( PhaseTimer );
+	GetWorld()->GetTimerManager().SetTimer(
+		PhaseTimer,
+		this,
+		&ATrafficLight::OnPhaseTimerTriggered,
+		GetCurrentPhaseDuration()
+	);
+
+	OnTrafficLightPhaseChanged( CurrentPhase );
+}
+
+ATrafficLight::ATrafficLight()
+{
+	PrimaryActorTick.bCanEverTick = true;
+	
+	StaticMesh = CreateDefaultSubobject< UStaticMeshComponent >( TEXT( "Static Mesh" ) );
+	RootComponent = StaticMesh;
+	
+	TrafficLightZone = CreateDefaultSubobject< UBoxComponent >( TEXT( "Traffic Light Zone" ) );
+	TrafficLightZone->SetupAttachment( RootComponent );
+}
+
+void ATrafficLight::BeginPlay()
+{
+	PhaseDurations = { RedDuration, RedAmberDuration, GreenDuration, AmberDuration };
+	CurrentPhase = StartingPhase;
+
+	if ( bAutoStart )
+		TurnOn();
+
+	Super::BeginPlay();
+}
+
+ETrafficLightPhase ATrafficLight::GetCurrentPhase()
+{
+	return CurrentPhase;
+}
+
+void ATrafficLight::TurnOn()
+{
+	if ( IsRunning() )
+		return;
+
+	CurrentPhase = GetPreviousPhase();
+	OnPhaseTimerTriggered();
+}
+
+void ATrafficLight::TurnOff()
+{
+	if ( !IsRunning() )
+		return;
+
+	GetWorld()->GetTimerManager().ClearTimer( PhaseTimer );
+}
+
+void ATrafficLight::Restart()
+{
+	if ( IsRunning() )
+	{
+		TurnOff();
+		TurnOn();
+	}
+}
+
+void ATrafficLight::SetPhaseDurations(float NewRedDuration, float NewRedAmberDuration, float NewGreenDuration,
+                                      float NewAmberDuration)
+{
+	PhaseDurations = { NewRedDuration, NewRedAmberDuration, NewGreenDuration, NewAmberDuration };
+	Restart();
+}
--- Source/VehicleTest/Public/TrafficRegulationActors/TrafficLight/TrafficLight.h
+++ Source/VehicleTest/Public/TrafficRegulationActors/TrafficLight/TrafficLight.h
@@ -5,6 +5,8 @@
 #include "CoreMinimal.h"
 #include "GameFramework/Actor.h"
 #include "Types.h"
+#include "Components/BoxComponent.h"
+#include "Components/ShapeComponent.h"
 #include "TrafficLight.generated.h"
 
 UCLASS()
@@ -12,10 +14,73 @@ class VEHICLETEST_API ATrafficLight : public AActor
 {
 	GENERATED_BODY()
 
-public:
-	// void SetTrafficLightPhase( ETrafficLightPhase NewTrafficLightPhase );
-	// void SwitchToNextPhase();
+	ETrafficLightPhase CurrentPhase = ETrafficLightPhase::Green;
 
+	FTimerHandle PhaseTimer;
+	
+	TArray< float > PhaseDurations = { 10.f, 2.f, 10.f, 2.f };
+	float GetCurrentPhaseDuration() const;
+	ETrafficLightPhase GetNextPhase() const;
+	ETrafficLightPhase GetPreviousPhase() const;
+
+	UFUNCTION()
+	void OnPhaseTimerTriggered();
+
+	/** Static mesh of the traffic light */
+	UPROPERTY( VisibleAnywhere, BlueprintReadOnly, meta = ( AllowPrivateAccess = "true" ) )
+	TObjectPtr< UStaticMeshComponent > StaticMesh;
+
+	/** Zone where the first cars should stop if the traffic light is red */
+	UPROPERTY( VisibleAnywhere, BlueprintReadOnly, meta = ( AllowPrivateAccess = "true" ) )
+	TObjectPtr< UBoxComponent > TrafficLightZone;
+
+public:
+	ATrafficLight();
+
+	virtual void BeginPlay() override;
+
+	/** Red phase duration */
+	UPROPERTY( EditAnywhere )
+	float RedDuration = 5.f;
+
+	/** Red-amber phase duration */
+	UPROPERTY( EditAnywhere )
+	float RedAmberDuration = 2.f;
+
+	/** Green phase duration */
+	UPROPERTY( EditAnywhere )
+	float GreenDuration = 5.f;
+
+	/** Amber phase duration */
+	UPROPERTY( EditAnywhere )
+	float AmberDuration = 2.f;
+
+	/** The phase the traffic light starts with */
+	UPROPERTY( EditAnywhere, BlueprintReadOnly )
+	ETrafficLightPhase StartingPhase = ETrafficLightPhase::Red;
+
+	/** Start the traffic light in BeginPlay()? */
+	UPROPERTY( EditAnywhere, BlueprintReadOnly )
+	bool bAutoStart = true;
+
+	/** Get current phase of the traffic light */
+	UFUNCTION( BlueprintPure )
+	ETrafficLightPhase GetCurrentPhase();
+
+	UFUNCTION( BlueprintCallable )
+	void TurnOn();
+
+	UFUNCTION( BlueprintCallable )
+	void TurnOff();
+
+	void Restart();
+
+	UFUNCTION( BlueprintCallable )
+	bool IsRunning() const;
+
+	UFUNCTION( BlueprintCallable )
+	void SetPhaseDurations( float NewRedDuration, float NewRedAmberDuration, float NewGreenDuration, float NewAmberDuration );
+	
 	UFUNCTION( BlueprintImplementableEvent )
 	void OnTrafficLightPhaseChanged( ETrafficLightPhase NewTrafficLightPhase );
 };
