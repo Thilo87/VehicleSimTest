1bd0795a5392efb3c83c6b6573f520e3d4f374de
Thilo <nollthilo@hotmail.com>
2024-02-05 17:19:41 +0100

    Created TrafficSimAiControlledCarKillVolume

---
+++ Source/VehicleTest/Private/TrafficSimulation/TrafficSimAiControlledCarKillVolume.cpp
@@ -0,0 +1,20 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "TrafficSimulation/TrafficSimAiControlledCarKillVolume.h"
+
+#include "Components/ShapeComponent.h"
+#include "TrafficSimulation/AiControlledCar/TrafficSimAiControlledCar.h"
+
+ATrafficSimAiControlledCarKillVolume::ATrafficSimAiControlledCarKillVolume()
+{
+	GetCollisionComponent()->SetCollisionProfileName( "OverlapAll" );
+}
+
+void ATrafficSimAiControlledCarKillVolume::NotifyActorBeginOverlap(AActor* OtherActor)
+{
+	Super::NotifyActorBeginOverlap( OtherActor );
+
+	if ( Cast< ATrafficSimAiControlledCar >( OtherActor ) )
+		OtherActor->Destroy();
+}
---
+++ Source/VehicleTest/Public/TrafficSimulation/TrafficSimAiControlledCarKillVolume.h
@@ -0,0 +1,22 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/TriggerBox.h"
+#include "UObject/Object.h"
+#include "TrafficSimAiControlledCarKillVolume.generated.h"
+
+/**
+ * Volume that destroys all AI-controlled cars that enter it
+ */
+UCLASS()
+class VEHICLETEST_API ATrafficSimAiControlledCarKillVolume : public ATriggerBox
+{
+	GENERATED_BODY()
+
+public:
+	ATrafficSimAiControlledCarKillVolume();
+	
+	virtual void NotifyActorBeginOverlap( AActor* OtherActor ) override;
+};
