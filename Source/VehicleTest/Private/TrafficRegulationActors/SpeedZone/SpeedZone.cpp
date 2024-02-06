9094a19c7e3b1104600ca9a8585ea72da24a8755
Thilo <nollthilo@hotmail.com>
2024-02-06 17:26:23 +0100

    Overworked SpeedZone, it's easier to use now; overworked main level

--- Source/VehicleTest/Private/TrafficRegulationActors/SpeedZone/SpeedZone.cpp
+++ Source/VehicleTest/Private/TrafficRegulationActors/SpeedZone/SpeedZone.cpp
@@ -7,10 +7,63 @@
 #include "PlayerControllers/VehicleTestPlayerController.h"
 #include "TrafficSimulation/AiControlledCar/TrafficSimAiControlledCar.h"
 
+void ASpeedZone::OnSpeedZoneBeginOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
+	UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	// if the other actor is AI-controlled, we don't need so much functionality
+	if ( ATrafficSimAiControlledCar* AiControlledCar = Cast< ATrafficSimAiControlledCar >( OtherActor ) )
+	{
+		AiControlledCar->OnEnteredSpeedZone( this );
+		return;
+	}
+
+	if ( !PlayerController.IsValid()
+		|| PlayerController->GetPawn() != OtherActor )
+			return;
+	
+	SetActorTickEnabled( true );
+	
+	bIsVehicleInSpeedZone = true;
+	PlayerController->OnEnteredSpeedZone( this );
+}
+
+void ASpeedZone::OnSpeedZoneEndOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
+	UPrimitiveComponent* OtherComp, int32 OtherBodyIndex)
+{
+	// if the other actor is AI-controlled, we don't need so much functionality
+	if ( ATrafficSimAiControlledCar* AiControlledCar = Cast< ATrafficSimAiControlledCar >( OtherActor ) )
+	{
+		AiControlledCar->OnLeftSpeedZone();
+		return;
+	}
+
+	if ( !PlayerController.IsValid()
+		|| PlayerController->GetPawn() != OtherActor )
+			return;
+
+	SetActorTickEnabled( false );
+	
+	bIsVehicleInSpeedZone = false;
+	PlayerController->OnLeftSpeedZone();
+
+	// we assume that if the player's vehicle left the speed zone, it's also "stopping to exceed the speed limit"
+	if ( bIsVehicleExceedingSpeedLimit )
+	{
+		bIsVehicleExceedingSpeedLimit = false;
+		PlayerController->OnStoppedExceedingSpeedLimit();
+	}
+}
+
 ASpeedZone::ASpeedZone()
 {
 	PrimaryActorTick.bCanEverTick = true;
 	PrimaryActorTick.bStartWithTickEnabled = false;
+
+	RootSceneComponent = CreateDefaultSubobject< USceneComponent >( "Root" );
+	RootComponent = RootSceneComponent;
+
+	Zone = CreateDefaultSubobject< UBoxComponent >( "Zone" );
+	Zone->SetupAttachment( RootSceneComponent );
 }
 
 void ASpeedZone::BeginPlay()
@@ -18,6 +71,9 @@ void ASpeedZone::BeginPlay()
 	Super::BeginPlay();
 
 	PlayerController = Cast< AVehicleTestPlayerController >( GetWorld()->GetFirstPlayerController() );
+
+	Zone->OnComponentBeginOverlap.AddDynamic( this, &ASpeedZone::OnSpeedZoneBeginOverlap );
+	Zone->OnComponentEndOverlap.AddDynamic( this, &ASpeedZone::OnSpeedZoneEndOverlap );
 }
 
 void ASpeedZone::Tick(float DeltaSeconds)
@@ -46,54 +102,5 @@ void ASpeedZone::Tick(float DeltaSeconds)
 				PlayerController->OnStoppedExceedingSpeedLimit();
 			}
 		}
-	}
-}
-
-void ASpeedZone::NotifyActorBeginOverlap(AActor* OtherActor)
-{
-	Super::NotifyActorBeginOverlap( OtherActor );
-
-	// if the other actor is AI-controlled, we don't need so much functionality
-	if ( ATrafficSimAiControlledCar* AiControlledCar = Cast< ATrafficSimAiControlledCar >( OtherActor ) )
-	{
-		AiControlledCar->OnEnteredSpeedZone( this );
-		return;
-	}
-
-	if ( !PlayerController.IsValid()
-		|| PlayerController->GetPawn() != OtherActor )
-		return;
-	
-	SetActorTickEnabled( true );
-	
-	bIsVehicleInSpeedZone = true;
-	PlayerController->OnEnteredSpeedZone( this );
-}
-
-void ASpeedZone::NotifyActorEndOverlap(AActor* OtherActor)
-{
-	Super::NotifyActorEndOverlap( OtherActor );
-
-	// if the other actor is AI-controlled, we don't need so much functionality
-	if ( ATrafficSimAiControlledCar* AiControlledCar = Cast< ATrafficSimAiControlledCar >( OtherActor ) )
-	{
-		AiControlledCar->OnLeftSpeedZone();
-		return;
-	}
-
-	if ( !PlayerController.IsValid()
-		|| PlayerController->GetPawn() != OtherActor )
-		return;
-
-	SetActorTickEnabled( false );
-	
-	bIsVehicleInSpeedZone = false;
-	PlayerController->OnLeftSpeedZone();
-
-	// we assume that if the player's vehicle left the speed zone, it's also "stopping to exceed the speed limit"
-	if ( bIsVehicleExceedingSpeedLimit )
-	{
-		bIsVehicleExceedingSpeedLimit = false;
-		PlayerController->OnStoppedExceedingSpeedLimit();
 	}
 }
--- Source/VehicleTest/Public/TrafficRegulationActors/SpeedZone/SpeedZone.h
+++ Source/VehicleTest/Public/TrafficRegulationActors/SpeedZone/SpeedZone.h
@@ -3,7 +3,7 @@
 #pragma once
 
 #include "CoreMinimal.h"
-#include "Engine/TriggerBox.h"
+#include "Components/BoxComponent.h"
 #include "UObject/Object.h"
 #include "SpeedZone.generated.h"
 
@@ -13,7 +13,7 @@ class AVehicleTestPlayerController;
  * Actor for a speed zone with a speed limit
  */
 UCLASS( Category = "Speed Zone" )
-class VEHICLETEST_API ASpeedZone : public ATriggerBox
+class VEHICLETEST_API ASpeedZone : public AActor
 {
 	GENERATED_BODY()
 
@@ -26,6 +26,20 @@ class VEHICLETEST_API ASpeedZone : public ATriggerBox
 	/** If the player's vehicle is currently in this speed zone */
 	bool bIsVehicleInSpeedZone = false;
 
+	/** Root scene component helper */
+	UPROPERTY( VisibleAnywhere, BlueprintReadOnly, meta = ( AllowPrivateAccess = "true" ), Category = "Speed Zone" )
+	TObjectPtr< USceneComponent > RootSceneComponent;
+	
+	/** Zone where the vehicles are bound to the speed limit */
+	UPROPERTY( VisibleAnywhere, BlueprintReadOnly, meta = ( AllowPrivateAccess = "true" ), Category = "Speed Zone" )
+	TObjectPtr< UBoxComponent > Zone;
+
+	UFUNCTION()
+	void OnSpeedZoneBeginOverlap( UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult );
+
+	UFUNCTION()
+	void OnSpeedZoneEndOverlap( UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex );
+
 public:
 	ASpeedZone();
 
@@ -36,6 +50,4 @@ public:
 	float SpeedLimit = 1388.89f;
 
 	virtual void Tick( float DeltaSeconds ) override;
-	virtual void NotifyActorBeginOverlap( AActor* OtherActor ) override;
-	virtual void NotifyActorEndOverlap( AActor* OtherActor ) override;
 };
