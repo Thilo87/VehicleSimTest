5ab212673b86f69e2bf1e9204df563bf49b5a505
Thilo <nollthilo@hotmail.com>
2024-02-06 18:14:38 +0100

    Overworked Types.h

--- Source/VehicleTest/Public/Types.h
+++ Source/VehicleTest/Public/Types.h
@@ -6,8 +6,12 @@
 
 #define PRINT_SCREEN( x ) if ( GEngine ) { GEngine->AddOnScreenDebugMessage( -1, 500.0f, FColor::Yellow, x ); }
 
+/** The name of the collision channel for obstacles */
 static FName OBSTACLE_COLLISION_CHANNEL_NAME = "Obstacle";
 
+/*
+ * Phases a traffic light can have
+ */
 UENUM( BlueprintType )
 enum class ETrafficLightPhase : uint8
 {
